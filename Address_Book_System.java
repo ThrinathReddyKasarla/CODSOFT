bf25693b27a36e3c78109e6429c38cecc50bdf15
Kasarla Thrinath Reddy <139155123+ThrinathReddyKasarla@users.noreply.github.com>
2023-08-14 17:49:09 +0530

    Add files via upload

---
+++ Address_Book_System.java
@@ -0,0 +1,120 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Contact {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Contact(String name, String phoneNumber, String emailAddress) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Phone: " + phoneNumber + ", Email: " + emailAddress;
+    }
+}
+
+class AddressBook {
+    private List<Contact> contacts;
+
+    public AddressBook() {
+        contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void removeContact(String name) {
+        contacts.removeIf(contact -> contact.getName().equals(name));
+    }
+
+    public Contact searchContact(String name) {
+        for (Contact contact : contacts) {
+            if (contact.getName().equals(name)) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    public void displayAllContacts() {
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+}
+
+public class Address_Book_System {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+
+        while (true) {
+            System.out.println("Address Book System Menu:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Remove Contact");
+            System.out.println("3. Search Contact");
+            System.out.println("4. Display All Contacts");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter Phone Number: ");
+                    String phoneNumber = scanner.nextLine();
+                    System.out.print("Enter Email Address: ");
+                    String emailAddress = scanner.nextLine();
+                    Contact newContact = new Contact(name, phoneNumber, emailAddress);
+                    addressBook.addContact(newContact);
+                    break;
+                case 2:
+                    System.out.print("Enter Name to Remove: ");
+                    String nameToRemove = scanner.nextLine();
+                    addressBook.removeContact(nameToRemove);
+                    break;
+                case 3:
+                    System.out.print("Enter Name to Search: ");
+                    String nameToSearch = scanner.nextLine();
+                    Contact searchedContact = addressBook.searchContact(nameToSearch);
+                    if (searchedContact != null) {
+                        System.out.println("Contact Found: " + searchedContact);
+                    } else {
+                        System.out.println("Contact Not Found.");
+                    }
+                    break;
+                case 4:
+                    addressBook.displayAllContacts();
+                    break;
+                case 5:
+                    System.out.println("Exiting");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
